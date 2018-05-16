554e5264c5c04bef8521d4f1d85a2e1135d4d311
matthew twyford <matthewtwyford@googlemail.com>
2018-05-15 20:05:07 -0400

    Signed-off-by: matthew twyford <matthewtwyford@googlemail.com>

--- src/app/title.component.ts
+++ src/app/title.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-title',
   template: `
-    <div class="container" id="work">
+    <div class="container pt-3 pb-5" id="{{ Title }}">
       <h2 class="display-4 text-center">{{ Title }}</h2>
        <span class="border d-block"></span>
     </div>
